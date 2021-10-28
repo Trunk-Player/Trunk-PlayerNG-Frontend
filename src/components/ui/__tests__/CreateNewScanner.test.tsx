d680d971941489b7a50b259efb325675471efd4d
Gabriel Graves <nebraskacoder@gmail.com>
2021-10-27 20:57:52 -0500

    Changed from Create-React-App to Create-Next-App

--- src/components/ui/__tests__/CreateNewScanner.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import CreateNewScanner from "components/ui/CreateNewScanner";
-
-test("renders create a new scanner text", () => {
-  render(<CreateNewScanner />);
-  const linkElement = screen.getByText(/Create a new scanner/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: "class", // or 'media' or 'class'
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
@@ -8,4 +9,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-};
+}
