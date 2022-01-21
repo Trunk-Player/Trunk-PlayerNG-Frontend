478fd5fda8acb0e81ca04002a095ac75fd5d5abe
Gabriel Graves <64073162+NebraskaCoder@users.noreply.github.com>
2022-01-21 02:42:23 +0000

    Remove instructions

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -2,14 +2,6 @@ import { ReactNode } from "react";
 import Sidebar from "components/sidebar";
 import Topbar from "components/topbar";
 
-/* This layout requires updating your template:
-  
-           ```
-           <html class="h-full bg-gray-100">
-           <body class="h-full">
-           ```
-*/
-
 export interface MainLayoutProps {
   children?: ReactNode;
 }
