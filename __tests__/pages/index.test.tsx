0a903e95aaa238eaed3f71159a0e6f4b35da34e9
Gabriel Graves <64073162+NebraskaCoder@users.noreply.github.com>
2022-04-02 04:29:04 +0000

    fix: build error for non page

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import TalkgroupsListPage from "../index";
+import TalkgroupsListPage from "pages/talkgroups/index";
 
 describe("Talkgroups List Page", () => {
   it("renders a table", () => {
