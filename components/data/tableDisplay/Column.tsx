9146a18da4b8db886e4ba7d175d1f623a2e58883
Gabriel Graves <64073162+NebraskaCoder@users.noreply.github.com>
2022-04-15 06:57:42 +0000

    feat: card and data components

---
+++ components/data/tableDisplay/Column.tsx
@@ -0,0 +1,48 @@
+import { ReactNode } from "react";
+import { classNames } from "utils/classNames";
+
+interface ColumnProps {
+  children: ReactNode;
+  className?: string;
+  heading?: boolean;
+  onUpdate?: () => void;
+}
+
+const Column = ({
+  children,
+  className,
+  onUpdate,
+  heading = false,
+}: ColumnProps) => {
+  return heading ? (
+    <dt
+      className={classNames(
+        className ?? "",
+        "text-sm font-medium text-gray-500"
+      )}
+    >
+      {children}
+    </dt>
+  ) : (
+    <dd
+      className={classNames(
+        className ?? "",
+        "mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2"
+      )}
+    >
+      <span className="flex-grow">{children}</span>
+      {onUpdate && (
+        <span className="ml-4 flex-shrink-0">
+          <button
+            type="button"
+            className="bg-white rounded-md font-medium text-cyan-600 hover:text-cyan-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
+            onClick={onUpdate}
+          >
+            Update
+          </button>
+        </span>
+      )}
+    </dd>
+  );
+};
+export default Column;
