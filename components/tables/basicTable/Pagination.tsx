5ab86a5932e49bdfd2a8877a5096a8ba0efa15ea
Gabriel Graves <64073162+NebraskaCoder@users.noreply.github.com>
2022-05-06 10:21:26 +0000

    feat: page information alignment

--- components/tables/basicTable/Pagination.tsx
+++ components/tables/basicTable/Pagination.tsx
@@ -34,14 +34,37 @@ const Pagination = ({
     onScrollToTopOfTable();
   };
 
+  const closeGoToPageModal = () => setGoToPageModalOpen(false);
+  const openGoToPageModal = () => setGoToPageModalOpen(true);
+
   const Separator = () => (
     <span className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700">
       ...
     </span>
   );
 
-  const closeGoToPageModal = () => setGoToPageModalOpen(false);
-  const openGoToPageModal = () => setGoToPageModalOpen(true);
+  const PageInformation = () => (
+    <div>
+      <p className="text-sm text-gray-700">
+        Showing{" "}
+        <span className="font-medium">{currentPage * limit - limit + 1}</span>{" "}
+        to{" "}
+        <span className="font-medium">
+          {currentPage === totalPages ? count : currentPage * limit}
+        </span>{" "}
+        of <span className="font-medium">{count}</span> results (
+        <button
+          className="font-medium underline"
+          onClick={() => {
+            openGoToPageModal();
+          }}
+        >
+          Go to page
+        </button>
+        )
+      </p>
+    </div>
+  );
 
   return (
     <>
@@ -53,7 +76,7 @@ const Pagination = ({
         onGoToPage={goToPage}
       />
       <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
-        <div className="flex-1 flex justify-between sm:hidden">
+        <div className="flex-1 flex justify-between sm:hidden items-center">
           {currentPage > 1 ? (
             <button
               className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
@@ -66,6 +89,7 @@ const Pagination = ({
               Previous
             </span>
           )}
+          <PageInformation />
           {currentPage < totalPages ? (
             <button
               className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
@@ -80,28 +104,7 @@ const Pagination = ({
           )}
         </div>
         <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
-          <div>
-            <p className="text-sm text-gray-700">
-              Showing{" "}
-              <span className="font-medium">
-                {currentPage * limit - limit + 1}
-              </span>{" "}
-              to{" "}
-              <span className="font-medium">
-                {currentPage === totalPages ? count : currentPage * limit}
-              </span>{" "}
-              of <span className="font-medium">{count}</span> results (
-              <button
-                className="font-medium underline"
-                onClick={() => {
-                  openGoToPageModal();
-                }}
-              >
-                Go to page
-              </button>
-              )
-            </p>
-          </div>
+          <PageInformation />
           <div>
             <nav
               className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
