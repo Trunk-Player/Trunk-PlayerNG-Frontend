d4b369905fabebaf3572127b02afef5c3987920b
Gabriel Graves <64073162+NebraskaCoder@users.noreply.github.com>
2022-05-28 00:42:23 +0000

    feat: lighter menu color

--- components/selectMenus/SelectMenuSimple.tsx
+++ components/selectMenus/SelectMenuSimple.tsx
@@ -40,11 +40,11 @@ const SelectMenuSimple = ({
           <div className="relative">
             <div className="inline-flex shadow-sm rounded-md divide-x divide-cyan-700">
               <div className="relative z-0 inline-flex shadow-sm rounded-md divide-x divide-cyan-700">
-                <div className="relative inline-flex items-center bg-cyan-600 py-2 pl-3 pr-4 border border-transparent rounded-l-md shadow-sm text-white">
+                <div className="relative inline-flex items-center bg-cyan-600 bg-opacity-75 py-2 pl-3 pr-4 border border-transparent rounded-l-md shadow-sm text-white">
                   <CheckIcon className="h-5 w-5" aria-hidden="true" />
                   <p className="ml-2.5 text-sm font-medium">{selected.title}</p>
                 </div>
-                <Listbox.Button className="relative inline-flex items-center bg-cyan-600 p-2 rounded-l-none rounded-r-md text-sm font-medium text-white hover:bg-cyan-700 focus:outline-none focus:z-10 focus:ring-0 focus:ring-transparent">
+                <Listbox.Button className="relative inline-flex items-center bg-cyan-600 bg-opacity-75 p-2 rounded-l-none rounded-r-md text-sm font-medium text-white hover:bg-cyan-700 hover:bg-opacity-75 focus:outline-none focus:z-10 focus:ring-0 focus:ring-transparent">
                   <span className="sr-only">Change published status</span>
                   <ChevronDownIcon
                     className="h-5 w-5 text-white"
