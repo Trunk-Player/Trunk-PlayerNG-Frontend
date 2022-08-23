81dade8f7d5d5ec5a119703f7e061ae272ff9ba5
Gabriel Graves <64073162+NebraskaCoder@users.noreply.github.com>
2022-08-23 05:58:04 +0000

    feat: add rr system id

--- types/api/System.ts
+++ types/api/System.ts
@@ -4,6 +4,7 @@ export interface System {
   UUID: string;
   name: string;
   systemACL: SystemACL;
+  rr_system_id?: string;
   enable_talkgroup_acls: boolean;
   prune_transmissions: boolean;
   prune_transmissions_after_days: number;
