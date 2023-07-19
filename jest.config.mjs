a89f13948fee3bde8d551e382b2fb0c2e63292cd
Gabriel Graves <nebraskacoder@gmail.com>
2023-07-19 04:45:12 +0000

    Jest and intl

---
+++ jest.config.mjs
@@ -0,0 +1,19 @@
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+    "^test-utils": "<rootDir>/@types/test-utils/test-utils.tsx",
+  },
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config);
