3703f040dd55260a876f4177dc0299471c3eef35
Gabriel Graves <nebraskacoder@gmail.com>
2024-03-08 05:15:35 +0000

    Move location of component stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,7 @@ const config: StorybookConfig = {
   stories: [
     "../stories/**/*.mdx",
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",
